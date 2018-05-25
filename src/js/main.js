0ded7c800577a5b8a22ce52368e5b54a699e858b
Wouter <wouter.deloof@gmail.com>
2018-05-25 17:09:26 +0200

    Added candidats.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
 "use strict";
 
+// Import header and footer into pages
+// Header
+const header = new XMLHttpRequest();
+header.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    document.querySelector(".header").innerHTML = this.responseText;
+  }
+};
+header.open("GET", "../header.html", true);
+header.send();
+
+// Footer
+const footer = new XMLHttpRequest();
+footer.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    document.querySelector(".footer").innerHTML = this.responseText;
+  }
+};
+footer.open("GET", "../footer.html", true);
+footer.send();
+
+// End import header and footer
+
 // Rotate slogan
 
 const phrases = ["des communes", "des provinces"];
