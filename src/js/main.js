aac9f8e9222ddcb6ac1c912dffcb889919b10d3f
Wouter <wouter.deloof@gmail.com>
2018-06-11 15:47:59 +0200

    Added youtube API: embedded movies now receive data from Youtube (titles, thumbnaisl, ...)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 ("use strict");
 
+//Debouncing variables
+let timeout = false;
+const delay = 250;
+
 // Import header and footer into pages
 // Header
 const header = new XMLHttpRequest();
@@ -31,7 +35,7 @@ headline.rotatePhrases();
 // End rotate slogan
 
 // Siema slider
-function siema(element, autoplay, draggable) {
+function siema(element, autoplay = true, draggable = true) {
   const sliderContainer = document.querySelector(".carousel");
 
   if (sliderContainer !== null) {
@@ -49,8 +53,7 @@ function siema(element, autoplay, draggable) {
     });
 
     // Create next/prev buttons
-    Siema.prototype.createButtons = function(e) {
-      console.log(e);
+    Siema.prototype.createButtons = function() {
       const nextButton = document.createElement("button");
       nextButton.classList.add("btn", "btn--carousel", "carousel__next");
       sliderContainer.appendChild(nextButton);
@@ -60,12 +63,26 @@ function siema(element, autoplay, draggable) {
       sliderContainer.appendChild(prevButton);
     };
 
-    slider.createButtons();
-    window.addEventListener("resize", () => slider.createButtons());
-
+    // Make buttons work
     const carousel = document.querySelector(`.${element}`);
-    const carouselPrev = document.querySelector(".carousel__prev");
-    const carouselNext = document.querySelector(".carousel__next");
+
+    Siema.prototype.bindButtons = function() {
+      const carouselPrev = document.querySelector(".carousel__prev");
+      const carouselNext = document.querySelector(".carousel__next");
+
+      carouselPrev.addEventListener("click", () => slider.prev());
+      carouselNext.addEventListener("click", () => slider.next());
+    };
+
+    slider.createButtons();
+    slider.bindButtons();
+    window.addEventListener("resize", () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        slider.createButtons();
+        slider.bindButtons();
+      }, delay);
+    });
 
     if (autoplay) {
       let autoSlide = setInterval(function() {
@@ -81,9 +98,6 @@ function siema(element, autoplay, draggable) {
           }, 5000))
       );
     }
-
-    carouselPrev.addEventListener("click", () => slider.prev());
-    carouselNext.addEventListener("click", () => slider.next());
   }
 }
 // End siema slider
@@ -91,12 +105,15 @@ function siema(element, autoplay, draggable) {
 // To top button
 const toTopElement = document.querySelector(".to-top__link");
 window.addEventListener("scroll", () => {
-  const fromTop = window.scrollY;
-  if (fromTop < 200) {
-    toTopElement.classList.remove("to-top__link--visible");
-  } else if (fromTop >= 200) {
-    toTopElement.classList.add("to-top__link--visible");
-  }
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    const fromTop = window.scrollY;
+    if (fromTop < 200) {
+      toTopElement.classList.remove("to-top__link--visible");
+    } else if (fromTop >= 200) {
+      toTopElement.classList.add("to-top__link--visible");
+    }
+  }, delay);
 });
 
 // Scroll to top animation
@@ -155,107 +172,121 @@ if (inputCandidates !== null) {
 
 // Working order: Load data from Youtube API => insert data in vidBoxTemplate => use template to render element on page
 
-const today = new Date();
-const dummy = {
-  viewCount: 4128,
-  likeCount: 1564,
-  dislikeCount: 54,
-  video: {
-    href: "https://www.youtube.com/watch?v=8JnfIa84TnU",
-    title: "Testing video"
-  },
-  date: `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`
-};
 // Load data from Youtube
-const data = dummy;
-
+const yt_api_key = "AIzaSyAXUGrOIVhDVVhldzfuPfTha2TDdolKMQk";
 const videos = document.querySelectorAll(".youtube");
 const mainEl = document.querySelector("main");
 
 videos.forEach(video => {
-  // Make list of video thumbnails from Youtube
+  // Request data from Youtube
   let id = video.dataset.href.split("=");
   id = id[id.length - 1];
-  video.style.background = `url(https://i.ytimg.com/vi/${id}/hqdefault.jpg) center center no-repeat`;
-  video.style.backgroundSize = "cover";
+  const yt_endpoint = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${id}&key=${yt_api_key}`;
 
-  // Open popup with video on click
-  video.addEventListener("click", e => {
-    // If element exists, remove it
-    const vidBox = document.querySelector(".video-box");
-    if (vidBox !== null) {
-      mainEl.removeChild(vidBox);
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const res = JSON.parse(this.responseText);
+
+      // Make list of video thumbnails from Youtube
+      video.style.background = `url(${
+        res.items[0].snippet.thumbnails.standard !== undefined
+          ? res.items[0].snippet.thumbnails.standard.url
+          : res.items[0].snippet.thumbnails.high.url
+      }) center center no-repeat`;
+      video.style.backgroundSize = "cover";
+
+      // Open popup with video on click
+      video.addEventListener("click", e => {
+        // If element exists, remove it
+        const vidBox = document.querySelector(".video-box");
+        if (vidBox !== null) {
+          mainEl.removeChild(vidBox);
+        }
+
+        createVidBox(res, video.dataset.href);
+
+        // Add close button functionality
+        const closeTrigger = document.querySelector(".video-box");
+        closeTrigger.addEventListener("click", function(e) {
+          document
+            .querySelector(".video-box")
+            .classList.add("video-box--close");
+          setTimeout(function() {
+            mainEl.removeChild(document.querySelector(".video-box"));
+          }, 1000);
+        });
+      });
     }
-
-    createVidBox(id, data);
-
-    // Add close button functionality
-    const closeTrigger = document.querySelector(".video-box");
-    closeTrigger.addEventListener("click", function(e) {
-      document.querySelector(".video-box").classList.add("video-box--close");
-      setTimeout(function() {
-        mainEl.removeChild(document.querySelector(".video-box"));
-      }, 1000);
-    });
-  });
+  };
+  xhttp.open("GET", yt_endpoint, true);
+  xhttp.send();
 });
 
-const createVidBox = (id, data) => {
+const createVidBox = (data, videoUrl) => {
+  data = data.items[0];
+  const date = new Date(data.snippet.publishedAt);
+  const publicationDate =
+    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+  console.log(data);
   const vidBoxTemplate = `
   <!-- close button -->
   <button class="video-box__close">X</button>
-  
+
   <!-- video -->
   <div class="video-box__video">
-    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${
+      data.id
+    }" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </div>
-  
+
   <!-- controls -->
   <div class="video-box__controls">
-  
+
     <!-- header -->
     <div class="video-box__header">
-      <h1 class="video-box__header__title">${data.video.title}</h1>
+      <h1 class="video-box__header__title">${data.snippet.title}</h1>
       <div class="video-box__header__stats">
         <h2 class="video-box__header__stats__views">
           <img src="assets/img/views.svg" alt="" class="video-box__header__stats__icon video-box__header__stats__icon--large"> ${
-            data.viewCount
+            data.statistics.viewCount
           }
         </h2>
-        <h6 class="video-box__header__stats__likes">
+        <h6 class="video-box__header__stats__likes ${
+          data.statistics.likeCount == undefined ? "u-hidden" : ""
+        }">
           <img src="assets/img/thumbs-up.svg" alt="" class="video-box__header__stats__icon"> ${
-            data.likeCount
+            data.statistics.likeCount
           }
         </h6>
-        <h6 class="video-box__header__stats__dislikes">
+        <h6 class="video-box__header__stats__dislikes ${
+          data.statistics.likeCount == undefined ? "u-hidden" : ""
+        }">
           <img src="assets/img/thumbs-down.svg" alt="" class="video-box__header__stats__icon"> ${
-            data.dislikeCount
+            data.statistics.dislikeCount
           }
         </h6>
       </div>
     </div>
-  
+
     <!-- meta -->
     <div class="video-box__meta">
     <div class="video-box__meta__subscribe"></div>
       <div class="video-box__meta__share">
-        <a href="http://www.facebook.com/sharer.php?u=${
-          data.video.href
-        }" class="video-box__meta__share__button video-box__meta__share__button--facebook">Share on Facebook</a>
+        <a href="http://www.facebook.com/sharer.php?u=${videoUrl}" class="video-box__meta__share__button video-box__meta__share__button--facebook">Share on Facebook</a>
         <a href="https://twitter.com/intent/tweet?text=${
-          data.video.title
-        }&url=${
-    data.video.href
-  }" class="video-box__meta__share__button video-box__meta__share__button--twitter">Share on Twitter</a>
+          data.snippet.title
+        }&url=${videoUrl}" class="video-box__meta__share__button video-box__meta__share__button--twitter">Share on Twitter</a>
       </div>
     </div>
-  
+
     <!-- Description -->
     <div class="video-box__description">
-      <p class="video-box__description__date">Ajoutée le ${data.date}
+      <p class="video-box__description__date">Ajoutée le ${publicationDate}
       </p>
-      <p class="video-box__description__description">Et error perferendis omnis dolor ea. Earum dolores non tenetur. Cumque velit deserunt aut et rerum. Sequi nostrum
-        culpa eum ratione et architecto quo qui autem. Qui minus debitis.</p>
+      <p class="video-box__description__description">${
+        data.snippet.description
+      }</p>
     </div>
   </div>
   `;
