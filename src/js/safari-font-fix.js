906988eda40438acbb9fb8334feca53c0545ce01
Wouter <wouter.deloof@gmail.com>
2018-06-13 10:24:42 +0200

    Added script to fix rendering of fonts in Safai browser

---
+++ src/js/safari-font-fix.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var is_chrome = navigator.userAgent.indexOf("Chrome") > -1;
+  var is_explorer = navigator.userAgent.indexOf("MSIE") > -1;
+  var is_firefox = navigator.userAgent.indexOf("Firefox") > -1;
+  var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+  var is_opera = navigator.userAgent.indexOf("Presto") > -1;
+  var is_mac = navigator.userAgent.indexOf("Mac OS") != -1;
+  var is_windows = !is_mac;
+
+  console.log(is_chrome, is_safari);
+
+  if (is_chrome && is_safari) {
+    is_safari = false;
+  }
+  console.log(is_chrome, is_safari);
+
+  if (is_safari || is_windows) {
+    document.querySelector("body").style.webkitTextStroke = "0.5px";
+  }
+  console.log(is_chrome, is_safari);
+});
